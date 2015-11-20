1777f4339516e1980ccf39d0d191ee38909ce091
gaumuel <gauthier.mueller@gmail.com>
2015-11-20 17:39:29 +0100

    delete files

--- debug-test.js
+++
@@ -1,8 +0,0 @@
-function myfunction(txt){
-	console.log(txt + 'Masiar is a bitch');
-	var myvar=2
-	myvar=myvar + 19;
-	console.log('done')
-}
-
-myfunction('Vassilis says, ');
--- start.sh
+++
@@ -1,1 +0,0 @@
-DEBUG=wls:root:connected,wls:node:*,wls:worker:output,wls:remote,wls:command,wls:root node koala_root.js 3000
