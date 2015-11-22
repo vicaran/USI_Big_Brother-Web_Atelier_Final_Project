943025fae0bde9d50b1299431035b041f93ebf20
Andrea <andreavicari95@gmail.com>
2015-11-22 16:18:03 +0100

    sockets

--- scripts/hasler_producer.js
+++ scripts/hasler_producer.js
@@ -9,17 +9,20 @@ var WebSocketServer = require('ws').Server
 
 wss.on('connection', function(socket){
 	console.log("New Connection");
+	console.log("This is socket:", socket)
 	socket.on("message", function(data){
 
 		console.log('This is the received data', data);
 		console.log("Bound: ",k.isBound());
 
-		if(k.isBound()){
-			data = JSON.parse(data);
-			console.log("Received:", data);
-			data.time = new Date().getTime();
-			k.send(data);
-		}
+
+
+		// if(k.isBound()){
+		// 	data = JSON.parse(data);
+		// 	console.log("Received:", data);
+		// 	data.time = new Date().getTime();
+		// 	k.send(data);
+		// }
 	});
 });
 
