1327991cbff6ba6180e9b4106afd5be669de505c
Andrea <andreavicari95@gmail.com>
2015-11-23 19:57:41 +0100

    Code.beautify()

--- scripts/tesselScripts/receiverWorker.js
+++ scripts/tesselScripts/receiverWorker.js
@@ -4,52 +4,41 @@ var ws = require("nodejs-websocket");
 var ports = 15000;
 
 // INSERT TESSEL IP ADDRESS HERE. Always prepend with 'ws://' to indicate websocket
-var connection = ws.connect('ws://10.40.2.139:' + ports, function () {
+var connection = ws.connect('ws://10.40.2.139:' + ports, function() {
     console.log('--Tessel Connected--')
 });
 
-
-var receiveData = function () {
+var receiveData = function() {
     var green = tessel.port['GPIO'].pin['G5'];
     var blue = tessel.port['GPIO'].pin['G4'];
     var red = tessel.port['GPIO'].pin['G6'];
     var yell = tessel.port['GPIO'].pin['G3'];
 
-    connection.on('text', function (data) {
-        var parse = JSON.parse(data)
-        var volume = parse.volume
+    connection.on('text', function(data) {
+        var parse = JSON.parse(data);
+        var volume = parse.volume;
+
         if (parse.light < 230) {
-            yell.write(1)
+            yell.write(1);
+        } else {
+            yell.write(0);
         }
 
         if (volume < 10) {
-            yell.write(0)
-            green.write(1)
-            blue.write(0)
-            red.write(0)
-
-        }
-        else if (volume >= 10 && volume <= 30) {
-            yell.write(0)
-            green.write(0)
-            blue.write(1)
-            red.write(0)
-
-        }
-        else if(volume > 30) {
-            yell.write(0)
-            green.write(0)
-            blue.write(0)
-            red.write(1)
-
+            green.write(1);
+            blue.write(0);
+            red.write(0);
+        } else if (volume >= 10 && volume <= 30) {
+            green.write(0);
+            blue.write(1);
+            red.write(0);
+        } else if (volume > 30) {
+            green.write(0);
+            blue.write(0);
+            red.write(1);
         }
 
     })
-
-    //
-    //interval = setInterval(function () {
-    //
-    //}, 500)
 };
 
 receiveData()
