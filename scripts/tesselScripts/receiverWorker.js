5c197cd1d6d2e61d41840304eb6f4b629c7d810b
Andrea <andreavicari95@gmail.com>
2015-11-26 16:12:44 +0100

    FUNZIONATUTTOGG

--- scripts/tesselScripts/receiverWorker.js
+++ scripts/tesselScripts/receiverWorker.js
@@ -15,37 +15,38 @@ var receiveData = function() {
     }, 4500)
 
     pws.receive(function(data) {
-        console.log(data,'DATA NON PARSATO DIO PRESEPE')
+        console.log(data, 'DATA NON PARSATO DIO PRESEPE')
         var parse = JSON.parse(data)
         console.log(parse)
-        if (parse == "RESET"){
+        if (parse == "RESET") {
             green.write(0);
             blue.write(0);
             red.write(0);
             yell.write(0);
-        }
-        var volume = parse.volume
-        if (parse.light < 230) {
-            yell.write(1)
-        }
-        if (parse.light >= 230) {
-            yell.write(0)
-        }
+        } else {
+            var volume = parse.volume
+            if (parse.light < 230) {
+                yell.write(1)
+            }
+            if (parse.light >= 230) {
+                yell.write(0)
+            }
 
-        if (volume < 10) {
-            green.write(1)
-            blue.write(0)
-            red.write(0)
+            if (volume < 10) {
+                green.write(1)
+                blue.write(0)
+                red.write(0)
 
-        } else if (volume >= 10 && volume <= 30) {
-            green.write(0)
-            blue.write(1)
-            red.write(0)
+            } else if (volume >= 10 && volume <= 30) {
+                green.write(0)
+                blue.write(1)
+                red.write(0)
 
-        } else if (volume > 30) {
-            green.write(0)
-            blue.write(0)
-            red.write(1)
+            } else if (volume > 30) {
+                green.write(0)
+                blue.write(0)
+                red.write(1)
+            }
         }
     })
 };
