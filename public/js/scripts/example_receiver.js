2b85ff455052a97f89228bc8ad4c6d94d0bbd53b
gaumuel <gauthier.mueller@gmail.com>
2015-11-27 16:23:16 +0100

    added graph

--- public/js/scripts/example_receiver.js
+++ public/js/scripts/example_receiver.js
@@ -5,13 +5,22 @@ var k = require('./../../../k_globals/koala.js')
 //script of the operator
 k.createNode(function(data) {
 	var data = JSON.parse(data);
-	k.callFunction("updateGraph", [data.content, data.time]);
+	var drawGraph = '';
+	if(data.header.type == "temp"){
+		drawGraph = "updateGraphTemp";
+	}
+	else if(data.header.type == "noise"){
+		drawGraph =  "updateGraphNoise";
+	}
+
+	k.callFunction(drawGraph, [data.content, data.time]);
 });
 
 //create the hidden div that will contain the received data
 k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
 k.createHTML('canvas', '<canvas id="canvas" width="500px" height="500px"></canvas>');
+k.createHTML('canvas', '<canvas id="canvas1" width="500px" height="500px"></canvas>');
 
 
 //add the graph script
-k.createScript('our_graph', 'js/ss_graph.js');
+k.createScript('our_graph', 'js/scripts/gau_graph.js');
--- scripts/example_producer.js
+++ scripts/example_producer.js
@@ -1,9 +1,20 @@
 var k = require('./../k_globals/koala.js')
 
 setInterval(function(msg) {
-	var data = {time: new Date().getTime() ,content: Math.floor((Math.random()*100)+1)};
+	var data = {
+		header: {position: "x", type: "temp"}, 
+		time: new Date().getTime(),
+		content: Math.floor((Math.random()*100)+1),
+		};
+		
+	var data1 = {
+		header: {position: "x", type: "noise"}, 
+		time: new Date().getTime(),
+		content: Math.floor((Math.random()*100)+1),
+		};
 
 	k.send(JSON.stringify(data));
+	k.send(JSON.stringify(data1));
 
 }, 1000);
 
