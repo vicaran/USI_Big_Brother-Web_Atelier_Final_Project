b6404393b05b707025b2261fa9a3cf1f649dc2af
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-11-27 16:34:46 +0100

    asdsda

---
+++ scripts/tesselScripts/temperatureProducerExample.js
@@ -0,0 +1,22 @@
+/**
+ * Created by VeaVictis on 27/11/15.
+ */
+
+var tessel = require('tessel');
+
+var main = function () {
+    var tempPin = tessel.port['GPIO'].pin['A1'];
+    var voltage = 0;
+    var temperature = 0.0
+    interval = setInterval(function () {
+        voltage = (tempPin.read() *3.3)/1024
+        console.log('voltage: ', voltage )
+
+        temperature = (voltage - 0.5) *100;
+        console.log(temperature)
+
+    }, 500)
+};
+
+
+main();
