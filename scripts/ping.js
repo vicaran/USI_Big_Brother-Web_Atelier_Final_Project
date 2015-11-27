077b1f509613af0705cf307cfeecfb17feb8eed8
gaumuel <gauthier.mueller@gmail.com>
2015-11-27 09:20:52 +0100

    playing with ping.js

--- scripts/ping.js
+++ scripts/ping.js
@@ -9,12 +9,9 @@ var k = require('./../k_globals/koala.js')
 setInterval(function(msg) {
 
 	k.send('ping');
-	k.createNode(function(m){
-		console.log(m);
-	});
 
 }, 1000);
 
 
-
-console.log('ping started')
+k.send('sending ping... ');
+console.log('ping started');
