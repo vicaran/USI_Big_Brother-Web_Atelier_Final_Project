5e0f08ee3a428b2d49c47ca6d407e0287c45de9b
Irene Jacob <irene.jacob@usi.ch>
2015-11-27 14:58:26 +0100

    added examples

---
+++ public/js/scripts/example_receiver.js
@@ -0,0 +1,16 @@
+//require WLS
+var k = require('./../../../k_globals/koala.js')
+
+
+//script of the operator
+k.createNode(function(data) {
+	k.callFunction("updateGraph", [data.content, data.time]);
+});
+
+//create the hidden div that will contain the received data
+k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
+k.createHTML('canvas', '<canvas id="canvas" width="500px" height="500px"></canvas>');
+
+
+//add the graph script
+k.createScript('our_graph', 'js/ss_graph.js');
