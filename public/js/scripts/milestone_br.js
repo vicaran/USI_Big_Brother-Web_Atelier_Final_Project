55bca6ba9ea7a3987367888877396c57793009ad
gaumuel <gauthier.mueller@gmail.com>
2015-11-29 20:01:05 +0100

    milestone charts

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -16,8 +16,8 @@ k.createNode(function(data) {
 //create the hidden div that will contain the received data
 k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
 k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
-k.createHTML('data', '<div id="newdata1" style="display:none;"> Hello</div>');
-k.createHTML('canvas', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
+k.createHTML('data1', '<div id="newdata1" style="display:none;"> Hello</div>');
+k.createHTML('canvas1', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
 
 
 
