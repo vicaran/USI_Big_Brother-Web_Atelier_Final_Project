410e4f2e866d8de15675039f9f2e16cd453efa14
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-11-29 16:26:10 +0100

    change thing

--- public/js/scripts/gau_worker.js
+++ public/js/scripts/gau_worker.js
@@ -1,28 +1,20 @@
-
 var pws = require("./producer_connection.js")
 
 /**
  * Main function that receiver the data from network add turn on some leds
  */
 
-var receiveData = function() {
+var receiveData = function () {
     pws.send('browser');
-    
-    pws.receive(function(data) {
-		console.log(data)
-    var data = JSON.parse(data);
-	var drawGraph = '';
-	if(data.header.type == "temp"){
-		drawGraph = "updateGraphTemp";
-	}
-	else if(data.header.type == "noise"){
-		drawGraph =  "updateGraphNoise";
-	}
 
-});
+    pws.receive(function (data) {
+        console.log(data)
+
+    })
+}
+
 
 //create the hidden div that will contain the received data
-    
-};
+
 
 receiveData();
