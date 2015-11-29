bfecfd855d1688b65d0f7c30794029a3e279fd1d
gaumuel <gauthier.mueller@gmail.com>
2015-11-29 14:58:46 +0100

    example

--- public/js/scripts/gau_ex.js
+++ public/js/scripts/gau_ex.js
@@ -17,7 +17,7 @@ var ht = {
 });
 
 //create the hidden div that will contain the received data
-k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
+k.createHTML('data', '<div id="newdata"></div>');
 k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
 k.createHTML('data1', '<div id="newdata1" style="display:none;"></div>');
 k.createHTML('canvas1', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
