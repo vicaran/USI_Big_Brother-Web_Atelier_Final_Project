d6a8af373222f3f5c49623bd2f5acc2f6c2c5b7e
gaumuel <gauthier.mueller@gmail.com>
2015-11-29 19:11:51 +0100

    milestone charts

---
+++ public/js/scripts/milestone_br.js
@@ -0,0 +1,23 @@
+//require WLS
+var k = require('./../../../k_globals/koala.js')
+
+
+//script of the operator
+k.createNode(function(data) {
+
+	
+	var data = JSON.parse(data);
+	console.log(data);
+	k.callFunction(updateGraph, [data.volume, data.light ,data.time]);
+
+});
+
+//create the hidden div that will contain the received data
+k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
+k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
+
+
+
+
+//add the graph script
+k.createScript('our_graph', 'js/scripts/milestone_graph.js');
