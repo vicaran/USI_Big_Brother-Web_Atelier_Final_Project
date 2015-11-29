d18d8047c2f957f32f245b3f6cc154aa52604687
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-11-29 16:48:40 +0100

    add au_worker socket

--- scripts/tesselScripts/gau_worker.js
+++ scripts/tesselScripts/gau_worker.js
@@ -6,11 +6,11 @@ var WebSocket = require('ws');
 var ws = new WebSocket('ws://195.176.181.55:15000');
 
 ws.on('open', function open() {
-    ws.send('BROWSER');
 });
 
 ws.on('message', function(data, flags) {
-    console.log(data)
+    console.log(data);
+
     // flags.binary will be set if a binary data is received.
     // flags.masked will be set if the data was masked.
 });
