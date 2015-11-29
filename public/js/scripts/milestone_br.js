e20fd4df6085f51b93c0c098817eb83c34f24747
gaumuel <gauthier.mueller@gmail.com>
2015-11-29 19:42:19 +0100

    milestone charts

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -8,14 +8,16 @@ k.createNode(function(data) {
 	
 	var data = JSON.parse(data);
 	console.log(data);
-	k.callFunction('updateGraph', [data.volume, data.light ,data.time]);
+	k.callFunction('updateGraphLine', [data.volume, data.light ,data.time]);
+	k.callFunction('updateGraphBar', [data.volume, data.light, data.time]);
 
 });
 
 //create the hidden div that will contain the received data
-k.createHTML('data', '<div id="newdata" style="display:none;"> Hello</div>');
+k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
 k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
-
+k.createHTML('data', '<div id="newdata1" style="display:none;"> Hello</div>');
+k.createHTML('canvas', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
 
 
 
--- public/js/scripts/milestone_graph.js
+++ public/js/scripts/milestone_graph.js
@@ -25,10 +25,39 @@ var lineChartData = {
 	]
 
 }
+
+
+var barChartData = {
+labels : [],
+datasets : [
+	{
+		label: "volume Data Set",
+		fillColor : "rgba(220,220,220,0.2)",
+		strokeColor : "rgba(220,220,220,1)",
+		highlightFill : "#fff",
+		highlightStroke : "rgba(220,220,220,1)",
+		data : []
+	},
+	{
+		label: "light Data Set",
+		fillColor : "rgba(151,187,205,0.2)",
+		strokeColor : "rgba(151,187,205,1)",
+		highlightFill : "#fff",
+		highlightStroke : "rgba(151,187,205,1)",
+		data : []
+	},
+]
+
+}
+
+
 var updateInterval;
 var myLine;
 var myRealLine;
+var myBar;
+var myRealBar;
 var ctx = document.getElementById("canvas").getContext("2d");
+var ctx_bar = document.getElementById("canvas1").getContext("2d");
 Chart.defaults.global.animation = false;
 Chart.defaults.global.showTooltips = false;
 
@@ -37,7 +66,12 @@ myRealLine = myLine.Line(lineChartData, {
 	responsive: true
 });
 
-var updateGraph = function(volume, light, time) {
+myBar = new Chart(ctx1);
+myRealBar = myBar.Bar(barChartData, {
+	barShowStroke: false
+	});
+
+var updateGraphLine = function(volume, light, time) {
 	console.log(volume,light, time)
 	var date = new Date(time).toUTCString();
 	date = date.split(' ')[4]
@@ -62,4 +96,29 @@ var updateGraph = function(volume, light, time) {
   //empty the content of the div
   document.getElementById('newdata').setAttribute('temperature', "");
   document.getElementById('newdata').setAttribute('time', "");
+}
+
+var updateGraphBar = function(volume, light, time) {
+	var date = new Date(time).toUTCString();
+	date = date.split(' ')[4]
+	myRealBar.destroy();
+		 
+  //push newly received data (time & data)
+  barChartData.datasets[0].data.push(volume);
+  barChartData.datasets[1].data.push(light);
+  barChartData.labels.push(date);
+
+  //if longer than 20, remove the first one
+  if(barChartData.datasets[0].data.length > 20 | barChartData.datasets[1].length > 20){
+    barChartData.datasets[0].data.shift();
+    barChartData.datasets[1].data.shift();
+    barChartData.labels.shift();
+  }
+
+  //draw it
+  myBar.Bar(barChartData);
+  
+  //empty the content of the div
+  document.getElementById('newdata1').setAttribute('noise', "");
+  document.getElementById('newdata1').setAttribute('time', "");
 }
