fb3c71759a21ceda72dcccd9e40083111ed175f9
gaumuel <gauthier.mueller@gmail.com>
2015-12-01 20:08:41 +0100

    clean public/js/scripts

--- public/js/scripts/join_c.js
+++
@@ -1,9 +0,0 @@
-/**
- * New node file
- */
-
-var k = require('./../k_globals/koala.js')
-
-k.createJoin(function(msg, uid) {
-	console.log(msg);
-});  
--- public/js/scripts/tc2.js
+++
@@ -1,9 +0,0 @@
-var k = require('./../k_globals/koala.js');
-
-//necessary to create Node?
-k.createNode(function(msg, uid) {
-	//k.storage.getMergedSortList("twitlist{0}","twitlist{1}","twitlist{2}"); 
-	//k.stateful("getMergedSortList", ["twitlist{0}","twitlist{1}","twitlist{2}"], function(r){console.log("res State from consumer::"+r) });
-	k.done();
-});
-
