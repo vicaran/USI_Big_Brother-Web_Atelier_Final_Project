ec8376829e3221d3c4600031fab3f1398a678842
Andrea <andreavicari95@gmail.com>
2015-12-03 17:21:38 +0100

    database.js

--- scripts/database.js
+++ scripts/database.js
@@ -3,13 +3,15 @@ var k = require('./../k_globals/koala.js')
 var addToDatabase = function(data, date) {
     var jsonParsed = JSON.parse(data);
     var valueJson = {
+    	"id" : 1;
         "volume": jsonParsed["volume"],
         "light": jsonParsed["light"],
         "temperature": 0
     }
     var keyDate = convertDate(date) + "-" + convertHour(date);
-    console.log('Data received for database:', valueJson);
-    console.log('And this is the date: ', keyDate);
+    console.log('Key to store in the database:', keyDate);
+    console.log('and this is the respective value:', valueJson);
+    
 
     k.stateful.set(keyDate, valueJson, function(){
     	console.log('Saved on the database');
