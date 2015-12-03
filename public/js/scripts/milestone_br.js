0d5b734b58f60afab143ae27faa7f33d27c80a38
Irene Jacob <irene.jacob@usi.ch>
2015-12-03 13:48:08 +0100

    changes

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -14,19 +14,19 @@ k.createNode(function(data) {
 });
 
 //create the hidden div that will contain the received data
-var htmlString = '<div id="graph-container">
-				<div id="newdata" style="display:none;"></div>
-				<canvas id="canvas" width="400px" height="400px"></canvas>
-				<div id="newdata1" style="display:none;"> Hello</div>
-				<canvas id="canvas1" width="400px" height="400px"></canvas>
-				</div>';
-k.createHTML('pimmi', htmlString);
-// k.createHTML('graph-container', '<div id="graph-container">');
-// k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
-// k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
-// k.createHTML('data1', '<div id="newdata1" style="display:none;"> Hello</div>');
-// k.createHTML('canvas1', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
-// k.createHTML('graph-container-close','</div>');
+// var htmlString = '<div id="graph-container">
+// 				<div id="newdata" style="display:none;"></div>
+// 				<canvas id="canvas" width="400px" height="400px"></canvas>
+// 				<div id="newdata1" style="display:none;"> Hello</div>
+// 				<canvas id="canvas1" width="400px" height="400px"></canvas>
+// 				</div>';
+// k.createHTML('pimmi', htmlString);
+k.createHTML('graph-container', '<div id="graph-container">');
+k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
+k.createHTML('canvas', '<canvas id="canvas" width="400px" height="400px"></canvas>');
+k.createHTML('data1', '<div id="newdata1" style="display:none;"> Hello</div>');
+k.createHTML('canvas1', '<canvas id="canvas1" width="400px" height="400px"></canvas>');
+k.createHTML('graph-container-close','</div>');
 
 //add the graph script
 k.createScript('our_graph', 'js/scripts/milestone_graph.js');
