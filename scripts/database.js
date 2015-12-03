14117412ba682d331d6b557c58dac05ce0347f11
Andrea <andreavicari95@gmail.com>
2015-12-03 16:07:39 +0100

    database date

--- scripts/database.js
+++ scripts/database.js
@@ -10,11 +10,22 @@ var addToDatabase = function(data, date) {
 }
 
 function convertDate(inputFormat) {
-    function pad(s) {
-        return (s < 10) ? '0' + s : s;
-    }
-    var d = new Date(inputFormat);
-    return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/');
+    // function pad(s) {
+    //     return (s < 10) ? '0' + s : s;
+    // }
+    // var d = new Date(inputFormat);
+    // return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/');
+    dformat = [(inputFormat.getMonth() + 1).padLeft(),
+        inputFormat.getDate().padLeft(),
+        inputFormat.getFullYear()
+    ].join('/') + ' ' +
+        [inputFormat.getHours().padLeft(),
+        inputFormat.getMinutes().padLeft(),
+        inputFormat.getSeconds().padLeft()
+    ].join(':');
+    return dformat;
 }
+
+
 
 exports.addToDatabase = addToDatabase;
