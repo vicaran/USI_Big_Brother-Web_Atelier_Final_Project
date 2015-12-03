92917731d213f3d81abecf759ac1d107dc9af421
Irene Jacob <irene.jacob@usi.ch>
2015-12-03 16:48:34 +0100

    meep

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -15,7 +15,7 @@ k.createNode(function(data) {
 
 //create the hidden div that will contain the received data
 var htmlString = '<div id="graph-container">' +
-				'<i class="fa fa-refresh"></i>' +
+				'<i id="arrow" class="fa fa-refresh"></i>' +
 				'<div id="newdata" style="display:none;"></div>' +
 				'<canvas id="canvas" width="400px" height="400px"></canvas>' +
 				'<div id="newdata1" style="display:none;"> Hello</div>' +
