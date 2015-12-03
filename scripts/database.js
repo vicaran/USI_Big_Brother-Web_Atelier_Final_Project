22b67e2d66c9ea6de48d6c6c738a0275465faee6
Andrea <andreavicari95@gmail.com>
2015-12-03 16:28:36 +0100

    date hour

--- scripts/database.js
+++ scripts/database.js
@@ -3,7 +3,7 @@ var k = require('./../k_globals/koala.js')
 var addToDatabase = function(data, date) {
     var jsonValue = JSON.parse(data);
     console.log('Data received:', jsonValue);
-    var keyDate = convertDate(date);
+    var keyDate = convertDate(date) + "-" + convertHour(date);
     console.log('And this is the date: ', keyDate);
 
     // k.stateful.set(key, value, cb);
@@ -13,10 +13,15 @@ function convertDate(inputFormat) {
     function pad(s) {
         return (s < 10) ? '0' + s : s;
     }
-    
     var d = new Date(inputFormat);
     datetext = d.toTimeString();
-    return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/'), datetext = datetext.split(' ')[0];
+    return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/');
+}
+
+function convertHour(inputFormat){
+	var d = new Date(inputFormat);
+    datetext = d.toTimeString();
+    return datetext = datetext.split(' ')[0];
 }
 
 exports.addToDatabase = addToDatabase;
