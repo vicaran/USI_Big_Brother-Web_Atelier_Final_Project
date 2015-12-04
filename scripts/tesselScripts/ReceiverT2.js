5dd73b897ffcf9e9f65f9e8fa8e843721f90ddb9
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-04 14:44:07 +0100

    adds

---
+++ scripts/tesselScripts/ReceiverT2.js
@@ -0,0 +1,53 @@
+/**
+ * Created by VeaVictis on 04/12/15.
+ */
+
+var TesselClasses = require('./TesselClasses.js');
+var ReceiverT2 = TesselClasses.ReceiveTessel(2);
+ReceiverT2.main = function(){
+    var self = this;
+    var green = tessel.port['GPIO'].pin['G5'];
+    var blue = tessel.port['GPIO'].pin['G4'];
+    var red = tessel.port['GPIO'].pin['G6'];
+    var yell = tessel.port['GPIO'].pin['G3'];
+    interval = setInterval(function() {
+        self.ws.ack()
+    }, 4500);
+
+    this.ws.receive(function(data) {
+        if (data == "RESET") {
+            green.write(0);
+            blue.write(0);
+            red.write(0);
+            yell.write(0);
+        } else {
+            var parse = JSON.parse(data)
+
+            var volume = parse.volume;
+            if (parse.light < 530) {
+                yell.write(1)
+            }
+            if (parse.light >= 230) {
+                yell.write(0)
+            }
+
+            if (volume < 10) {
+                green.write(1)
+                blue.write(0)
+                red.write(0)
+
+            } else if (volume >= 10 && volume <= 30) {
+                green.write(0)
+                blue.write(1)
+                red.write(0)
+
+            } else if (volume > 30) {
+                green.write(0)
+                blue.write(0)
+                red.write(1)
+            }
+        }
+    })
+};
+
+ReceiverT2.start();
