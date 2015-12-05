3d3645f2d40be30c0b69d0d8dccf2d7168dad915
Irene Jacob <irene.jacob@usi.ch>
2015-12-05 21:32:46 +0100

    switching is stupid

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -24,7 +24,7 @@ var legend = '<div class="key">'+
 var htmlString = 
 '<div id="navbar">' +
     		'<div class="nav-panel">' +
-    			'<div id="home" class="fa fa-home">&nbsp; HOME</div>' +
+    			'<div id="home" class="fa fa-home">HOME</div>' +
     			'<div class="nav-line"></div>' +
     		'</div>' +
     		'<div class="nav-panel">' +
@@ -36,14 +36,11 @@ var htmlString =
     			'<div class="nav-line"></div>' +
     		'</div>' +
     		'<div class="nav-panel">' +
-    			'<div id="activate-lights" class="fa fa-lightbulb-o">&nbsp; ACTIVATE LIGHTS</div>' +
+    			'<div id="activate-lights" class="fa fa-lightbulb-o">ACTIVATE LIGHTS</div>' +
     			'<div class="nav-line"></div>' +
     		'</div>' +
     	'</div>' +
-    	'<div id="homepage" > This is the Home <div>' +
-    	'<div id="old-graphs"  class="hidden"> Please choose a date and time<div>' +
-		'<div id="light-bulb"  class="hidden"><i class="fa fa-lightbulb-o fa-5x fa-pulse"></i><div>' +
-    	'<div id="graph-container" class="hidden" >' +
+    	'<div id="graph-container">' +
 				'<span id="arrow" class="fa fa-refresh" ></span>' +
 				'<div id="newdata" style="display:none;"></div>' +
 				'<div id="c" class="">' + 
@@ -55,7 +52,7 @@ var htmlString =
  				'<canvas id="canvas1"  width="400px" height="400px"></canvas>' +
  				legend +
 				'</div>' +
-		'</div>';
+				'</div>';
 k.createHTML('pimmi', htmlString);
 // k.createHTML('graph-container', '<div id="graph-container">');
 // k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
