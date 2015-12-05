0f53593ba020581364f8f3ab67fcd184ca00fcbc
Irene Jacob <irene.jacob@usi.ch>
2015-12-05 22:39:46 +0100

    trying the switching again

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -21,40 +21,38 @@ var legend = '<div class="key">'+
             	'<div class="key_name"><div>Volume</div>'+
             	'<div>Light</div></div>'+
         	'</div>'
-var htmlString = '<div id="navbar">'+
-            '<div class="nav-panel">'+
-                '<div id="home" class="fa fa-home">&nbsp; HOME</div>'+
-                '<div class="nav-line"></div>'+
-            '</div>'+
-            '<div class="nav-panel">'+
-                '<div id="live-feed" class="fa">LIVE FEED</div>'+
-                '<div class="nav-line"></div>'+
-            '</div>'+
-            '<div class="nav-panel">'+
-                '<div id="archives" class="fa">ARCHIVES</div>'+
-                '<div class="nav-line"></div>'+
-            '</div>'+
-            '<div class="nav-panel">'+
-                '<div id="activate-lights" class="fa fa-lightbulb-o">&nbsp; ACTIVATE LIGHTS</div>'+
-                '<div class="nav-line"></div>'+
-            '</div>'+
-        '</div>'+
-       '<div id="homepage" > This is the Home </div>'+
-        '<div id="old-graphs"  class="hidden"> Please choose a date and time</div>'+
-        '<div id="light-bulb"  class="hidden"><i class="fa fa-lightbulb-o fa-5x fa-pulse"></i></div>'+
-                    <'div id="graph-container" class="hidden" >'+
-                        '<span id="arrow" class="fa fa-refresh" ></span>'+
-                        '<div id="newdata" style="display:none;"></div>'+
-                        '<div id="c" class="">'+
-                            '<canvas id="canvas"  width="400px" height="400px"></canvas>'+
-                            legend+
-                        '</div>'+
-                        '<div id="newdata1" style="display:none;"> Hello</div>'+
-                        '<div id="c1" class="">'+
-                            '<canvas id="canvas1"  width="400px" height="400px"></canvas>'+
-                            legend+
-                        '</div>'+
-       ' </div>';
+var htmlString = 
+'<div id="navbar">' +
+    		'<div class="nav-panel">' +
+    			'<div id="home" class="fa fa-home">&nbsp; HOME</div>' +
+    			'<div class="nav-line"></div>' +
+    		'</div>' +
+    		'<div class="nav-panel">' +
+    			'<div id="live-feed" class="fa">LIVE FEED</div>' +
+    			'<div class="nav-line"></div>' +
+    		'</div>' +
+    		'<div class="nav-panel">' +
+    			'<div id="archives" class="fa">ARCHIVES</div>' +
+    			'<div class="nav-line"></div>' +
+    		'</div>' +
+    		'<div class="nav-panel">' +
+    			'<div id="activate-lights" class="fa fa-lightbulb-o">&nbsp; ACTIVATE LIGHTS</div>' +
+    			'<div class="nav-line"></div>' +
+    		'</div>' +
+    	'</div>' +
+    	'<div id="graph-container">' +
+				'<span id="arrow" class="fa fa-refresh" ></span>' +
+				'<div id="newdata" style="display:none;"></div>' +
+				'<div id="c" class="">' + 
+				'<canvas id="canvas"  width="400px" height="400px"></canvas>' +
+				legend + 
+				'</div>' +
+				'<div id="newdata1" style="display:none;"> Hello</div>' +
+				'<div id="c1" class="">' +
+ 				'<canvas id="canvas1"  width="400px" height="400px"></canvas>' +
+ 				legend +
+				'</div>' +
+				'</div>';
 k.createHTML('pimmi', htmlString);
 // k.createHTML('graph-container', '<div id="graph-container">');
 // k.createHTML('data', '<div id="newdata" style="display:none;"></div>');
