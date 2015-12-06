dc7a5db5215338426a558c007f712e6a730cbeae
gaumuel <gauthier.mueller@gmail.com>
2015-12-06 19:22:58 +0100

    4 graphs

--- public/js/scripts/g_v.js
+++ public/js/scripts/g_v.js
@@ -23,7 +23,7 @@ k.createNode(function(data) {
 });
 
 //create the hidden div that will contain the received data
-var htmlString =//'<input name="fromDate" type="text" value="Insert date from to" id="inputDate"></input>'+
+var htmlString ='<input name="fromDate" type="text" value="Insert date from to" id="inputDate"></input>'+
                 //'<button id="button">click me</button>'+
 				'<div id="graph-container">' +
 				'<div id="newdata0" style="display:none;"></div>' +
