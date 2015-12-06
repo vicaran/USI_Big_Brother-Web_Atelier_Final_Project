a6ae5d9900986f9675b321d387ec3432bdf23b00
gaumuel <gauthier.mueller@gmail.com>
2015-12-06 08:10:54 +0100

    try sendind request

--- public/js/scripts/g_v.js
+++ public/js/scripts/g_v.js
@@ -14,8 +14,8 @@ k.createNode(function(data) {
 });
 
 //create the hidden div that will contain the received data
-var htmlString ='<input name="fromDate" type="text" value="Insert date from to" id="inputDate"></input>'+
-				'<div id="graph-container">' +
+var htmlString ='<div id="graph-container">' +
+				'<input name="fromDate" type="text" value="Insert..." id="inputDate"></input>'+
 				'<div id="radarData" style="display:none;"></div>' +
 				'<canvas id="canvasRadar" width="400px" height="400px"></canvas>' +
 				'<div id="newdata" style="display:none;"></div>' +
