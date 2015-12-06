7ee6d0933dfd904d6f659b62c91da4742aa8427b
Andrea <andreavicari95@gmail.com>
2015-12-06 19:51:56 +0100

    image

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -64,7 +64,7 @@ var htmlString =
     	'<div id="light-bulb" class="hidden">'+
     	// Here we make the buttons for the tessel
     	// ---Andrea's Implementation :)
-		  '<a href="#" bubbletooltip="Light is currently: ON"><img src="./../part1.png" alt="p1" class="setDimension"></a>' +
+		  '<a href="#" bubbletooltip="Light is currently: ON"><img src="http://i67.tinypic.com/bg5ovs.png" alt="p1" class="setDimension"></a>' +
    '<a href="#" bubbletooltip="Light is currently: OFF"><img src="./../part2.png" alt="p2" class="setDimension"></a>' +
    '<a href="#" bubbletooltip="Light is currently: OFF"><img src="./../part3.png" alt="p3" class="setDimension"></a>' +
 		// ---------------------------
