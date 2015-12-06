6211685f69377e152288f6ff94b7668d04b67476
Irene Jacob <irene.jacob@usi.ch>
2015-12-06 18:44:34 +0100

    database section card

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -57,8 +57,8 @@ var htmlString =
     	// DATABASE GRAPHS
     	'<div id="old-graphs" class="hidden">'+
     	'<div class="fa card">'+
-    	'<div class="postpone">KEEP WATCHING TO FIND OUT...</div>'+
-    	'</div><i class="fa fa-exclamation-triangle fa-5x" style="color: #871F17"></i>'+
+    	'<div class="postpone">KEEP WATCHING TO FIND OUT...<br><i class="fa fa-exclamation-triangle fa-5x" style="color: #871F17"></i></div>'+
+    	'</div>'+
     	'</div>' +
     	// ACTIVATE LIGHTS SECTION
     	'<div id="light-bulb" class="hidden">'+
