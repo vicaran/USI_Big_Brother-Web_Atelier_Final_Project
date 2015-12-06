586b86a4d15d35ba39aa50ec03a4a1c87a60ad71
Irene Jacob <irene.jacob@usi.ch>
2015-12-06 01:47:17 +0100

    fixing header

--- public/js/scripts/milestone_br.js
+++ public/js/scripts/milestone_br.js
@@ -40,6 +40,7 @@ var htmlString =
     			'<div class="nav-line"></div>' +
     		'</div>' +
     	'</div>' +
+    	'<div class="whitespace"></div>' + 
     	'<div id="info-page" class="hidden">BIG BROTHER IS WATCHING U</div>' +
     	'<div id="old-graphs" class="hidden">This is where we add the database stuff</div>' +
     	'<div id="light-bulb" class="hidden">Turn USI lights on and off</div>' +
