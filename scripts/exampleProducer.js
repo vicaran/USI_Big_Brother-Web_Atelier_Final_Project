e91cfa9c684feda2ef68fb4ea315c082a08c7037
Irene Jacob <irene.jacob@usi.ch>
2015-12-08 20:43:10 +0100

    i forgot what this was

--- scripts/exampleProducer.js
+++ scripts/exampleProducer.js
@@ -13,12 +13,12 @@ wss.on('connection', function(ws) {
     ws.on('message', function(data) {
 
         console.log('Here is data:', data);
-    })
-})
+    });
+});
 
 setInterval(function(msg) {
 	var data = {
-		header: {position: "x", type: "temp"}, 
+		header: {position: "x", type: "temp"},
 		time: new Date().getTime(),
 		content: Math.floor((Math.random()*100)+1),
 		};
