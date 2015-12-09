8e328c0022b1d7df0f589103a32dd7555b48e456
gaumuel <gauthier.mueller@gmail.com>
2015-12-09 13:59:38 +0100

    tt

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -1,16 +1,10 @@
 var k = require('./../k_globals/koala.js')
 
-var data = {header: "browser", content: "hello"};
-
-setInterval(function(msg) {
-
-	k.send(JSON.stringify(data));
-
-}, 1000);
-
+var data = {header: "browser", content: "data from database"};
 
 k.createNode(function(msg){
-	console.log("createNode " + msg);	
+	console.log("createNode " + msg);
+	k.send(JSON.stringify(data));	
 });
 
-console.log('Sending data... ');
+console.log('waiting for request');
