d6a5e0191cad0cbda557c3420c01ef6cb6caae0c
gaumuel <gauthier.mueller@gmail.com>
2015-12-10 15:29:17 +0100

    retireve from database

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -1,9 +1,12 @@
 var k = require('./../k_globals/koala.js')
+var dataB = require('./database.js')
 
 var data = {header: "database", content: "data from database"};
 
 k.createNode(function(msg){
 	console.log("createNode " + msg);
+	d = dataB.retrieveData(1449676695680,1449676698689);
+	console.log(d)
 	k.send(JSON.stringify(data));	
 });
 
