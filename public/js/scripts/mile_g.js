25405da7f1d2670b965658db5b4bb4c70289ab27
Irene Jacob <irene.jacob@usi.ch>
2015-12-10 15:57:36 +0100

    nav bar

--- public/js/scripts/mile_g.js
+++ public/js/scripts/mile_g.js
@@ -125,7 +125,7 @@ document.getElementById('live-feed').parentNode.lastChild.setAttribute('style', 
 var nav = document.getElementById('navbar');
 nav.addEventListener('click', function(e) {
     // console.log('e');
-    console.log(e.target.firsChild);
+    console.log(e.target.childNodes);
     if (e.target.id == "about" || e.target.firsChild.id == "about") {
         document.getElementById('info-page').setAttribute('class', '');
         document.getElementById('graph-container').setAttribute('class', 'hidden');
