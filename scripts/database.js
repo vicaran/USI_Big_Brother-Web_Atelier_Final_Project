407356b747cfe71380557591dbe2ce8644c3cfef
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-11 21:18:48 +0100

    ok

--- scripts/database.js
+++ scripts/database.js
@@ -45,14 +45,13 @@ var addToDatabase = function (data, d) {
  * @param to The finish point
  */
  var retrieveData = function (id, since, to) {
-    var sSince = convertMStoS(since);
-    var sTo = convertMStoS(to);
-
     k.stateful.get(id, function (res) {
         var parse = JSON.parse(res);
-        var oldest = convertMStoS(parse[0].time);
-        var startPoint =  convertMStoS(since) - oldest;
-        var finishPoint = sTo - sSince;
+        var oldest = parse[0].time;
+        var startPoint =  since - oldest;
+        startPoint = convertMStoS(startPoint)
+        var finishPoint = to - since;
+        finishPoint = convertMStoS(finishPoint)
         var toRetrieveData = [];
         var i = startPoint;
         console.log("Startpoint: ", startPoint, " finishPoint: ", finishPoint, " index i: ", i, " oldest ", oldest);
