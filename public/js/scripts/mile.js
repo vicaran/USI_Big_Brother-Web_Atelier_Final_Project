26630496d80dd36041a77f9f081a049e3806f328
Andrea <andreavicari95@gmail.com>
2015-12-11 15:11:06 +0100

    switche separate

--- public/js/scripts/mile.js
+++ public/js/scripts/mile.js
@@ -54,10 +54,6 @@ var about_section = '<div class="fa card">'+
 'Donec molestie maximus velit, vitae venenatis massa tristique non. Aliquam erat volutpat. Duis imperdiet tempor elit ut sollicitudin. Fusce at purus vehicula, venenatis libero sit amet, faucibus eros. Maecenas id aliquam magna. Maecenas scelerisque sapien a metus iaculis facilisis. Duis tincidunt scelerisque sem et tempus. Phasellus iaculis tellus in ex pretium, a pellentesque justo pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam bibendum aliquet dui, sollicitudin pharetra lectus dapibus in. Nullam porta erat ut pellentesque tristique.</div>'+
 '</div>'
 
-
-
-
-
 var legend = '<div class="key">'+
             	'<div style="padding-left: 11px">LEGEND</div>'+
             	'<div class="key_entry">'+
@@ -71,11 +67,6 @@ var legend = '<div class="key">'+
                 '<div>Temp</div>'+
                 '</div>'+
         	'</div>'
-
-
-
-
-
 
 var htmlString = 
 // HEADER
@@ -171,26 +162,26 @@ var htmlString =
         '</canvas>'+
         // '<button class="fa fa-search search-button" onclick="turnOnLight(1)"></button>'+
         '<div class="onoffswitch">'+
-            '<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>'+
-            '<label class="onoffswitch-label" for="myonoffswitch">'+
-                '<span class="onoffswitch-inner"></span>'+
-                '<span class="onoffswitch-switch"></span>'+
+            '<input type="checkbox" name="onoffswitch1" class="onoffswitch-checkbox1" id="myonoffswitch1" checked>'+
+            '<label class="onoffswitch-label1" for="myonoffswitch1">'+
+                '<span class="onoffswitch-inner1"></span>'+
+                '<span class="onoffswitch-switch1"></span>'+
             '</label>'+
         '</div>'+
 
         '<div class="onoffswitch">'+
-            '<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>'+
-            '<label class="onoffswitch-label" for="myonoffswitch">'+
-                '<span class="onoffswitch-inner"></span>'+
-                '<span class="onoffswitch-switch"></span>'+
+            '<input type="checkbox" name="onoffswitch2" class="onoffswitch-checkbox2" id="myonoffswitch2" checked>'+
+            '<label class="onoffswitch-label2" for="myonoffswitch2">'+
+                '<span class="onoffswitch-inner2"></span>'+
+                '<span class="onoffswitch-switch2"></span>'+
             '</label>'+
         '</div>'+
 
         '<div class="onoffswitch">'+
-            '<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>'+
-            '<label class="onoffswitch-label" for="myonoffswitch">'+
-                '<span class="onoffswitch-inner"></span>'+
-                '<span class="onoffswitch-switch"></span>'+
+            '<input type="checkbox" name="onoffswitch3" class="onoffswitch-checkbox3" id="myonoffswitch3" checked>'+
+            '<label class="onoffswitch-label3" for="myonoffswitch3">'+
+                '<span class="onoffswitch-inner3"></span>'+
+                '<span class="onoffswitch-switch3"></span>'+
             '</label>'+
         '</div>'+
 		// ---------------------------
