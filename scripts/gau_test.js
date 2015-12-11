b78155f23f38e6907317e257d55a666b2ede0921
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-11 20:03:57 +0100

    asd

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -10,12 +10,9 @@ k.createNode(function(msg){
 
 	dataB.retrieveData(msg.id, msg.from, msg.to);
 
-<<<<<<< HEAD
 	dataB.retrieveData(msg.id, 1449860373114,1449860383144)
-=======
 	// dataB.retrieveData(msg.id, 1449676695680,1449676698689)
->>>>>>> origin/master
-	//k.send(JSON.stringify(data));	
+	//k.send(JSON.stringify(data));
 });
 
 console.log('waiting for request');
