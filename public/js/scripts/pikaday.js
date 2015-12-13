887c541e27ceaebebe805ae14290c5528378e4e9
gaumuel <gauthier.mueller@gmail.com>
2015-12-13 14:42:20 +0100

    pikaday and moment.js

--- public/js/scripts/pikaday.js
+++ public/js/scripts/pikaday.js
@@ -4313,50 +4313,28 @@
 
 
 
-var initialDateFromDatabase = 1449953366430//Get from database
+
 for(var i = 1; i<4; i++){
-     var startDate, endDate,
-        updateStartDate = function() {
-            startPicker.setStartRange(startDate);
-            endPicker.setStartRange(startDate);
-            endPicker.setMinDate(startDate);
-        },
-        updateEndDate = function() {
-            startPicker.setEndRange(endDate);
-            startPicker.setMaxDate(endDate);
-            endPicker.setEndRange(endDate);
-        },
-        startPicker = new Pikaday({
-            field: document.getElementById('db-from'+i),
-            minDate: new Date(initialDateFromDatabase),
-            maxDate: new Date(2020, 12, 31),
-            onSelect: function() {
-                console.log(this.getDate())
-                startDate = this.getDate();
-                updateStartDate();
-            }
-        }),
-        endPicker = new Pikaday({
-            field: document.getElementById('db-to'+i),
-            minDate: new Date(initialDateFromDatabase),
-            maxDate: new Date(2020, 12, 31),
-            onSelect: function() {
-                endDate = this.getDate();
-                updateEndDate();
-            }
-        }),
-        _startDate = startPicker.getDate(),
-        _endDate = endPicker.getDate();
+var pickerStart = new Pikaday(
+    {
+        field: document.getElementById('db-from'+i),
+        firstDay: 1,
+        format: 'MM/DD/YYYY',
+        minDate: new Date(2000, 0, 1),
+        maxDate: new Date(2020, 12, 31),
+        yearRange: [2000,2020]
+    });
 
-        if (_startDate) {
-            startDate = _startDate;
-            updateStartDate();
-        }
-
-        if (_endDate) {
-            endDate = _endDate;
-            updateEndDate();
-        }
+var pickerEnd = new Pikaday(
+    {
+        field: document.getElementById('db-to'+i),
+        firstDay: 1,
+        format: 'MM/DD/YYYY',
+        minDate: new Date(2000, 0, 1),
+        maxDate: new Date(2020, 12, 31),
+        yearRange: [2000,2020]
+    });
+}
 
 
 
