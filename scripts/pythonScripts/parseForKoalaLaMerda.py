f30064f127982a4dd1c3f92ff042198166655dd0
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-13 18:45:19 +0100

    result

--- scripts/pythonScripts/parseForKoalaLaMerda.py
+++ scripts/pythonScripts/parseForKoalaLaMerda.py
@@ -2,8 +2,8 @@ __author__ = 'VeaVictis'
 
 
 f = open('testhtml.txt', 'r')
+result = open('result.txt','w')
 newString = ''
 for line in f:
-  newString += "'"+line+"'+"
+  result.write("'"+line+"' +")
 
-print(newString)
