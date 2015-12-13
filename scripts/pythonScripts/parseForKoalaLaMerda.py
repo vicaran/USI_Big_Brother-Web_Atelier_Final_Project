c7488a42423b65f09ce7a73f6609ee3308f2ac46
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-13 19:12:39 +0100

    asd

--- scripts/pythonScripts/parseForKoalaLaMerda.py
+++ scripts/pythonScripts/parseForKoalaLaMerda.py
@@ -1,7 +1,7 @@
 __author__ = 'VeaVictis'
 
 
-f = open('../../public/js/scripts/miracolo/index.html', 'r')
+f = open('../../public/js/scripts/index.html', 'r')
 result = open('result.txt','w')
 newString = ''
 for line in f:
