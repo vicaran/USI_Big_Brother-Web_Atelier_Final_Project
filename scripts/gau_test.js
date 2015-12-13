568ab01f8c48a446454ba11b5624b9419f8f368c
gaumuel <gauthier.mueller@gmail.com>
2015-12-13 14:40:31 +0100

    pikaday and moment.js

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -2,7 +2,6 @@ var k = require('./../k_globals/koala.js')
 var dataB = require('./database.js')
 
 //var data = {header: "database", content: "data from database"};
-
 k.createNode(function(msg){
 	console.log("createNode " + msg);
 	msg = JSON.parse(msg);
