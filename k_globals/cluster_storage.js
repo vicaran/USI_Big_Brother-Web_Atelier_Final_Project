761c9363bbc496ac48971dcc09cabccb98e49c53
vicaran <andreavicari95@gmail.com>
2015-12-13 22:16:50 +0100

    prova

--- k_globals/cluster_storage.js
+++ k_globals/cluster_storage.js
@@ -3,35 +3,52 @@ var log = debug('wls:davide');
 var flatten = require('flat');
 var unflatten = require('flat').unflatten;
 var redis = require('redis');
+//var redis = require('thunk-redis');
 var Thunk = require('thunks')();
-var Promise = require('bluebird');
+
 var client = redis.createClient();
 //var client = redis.createClient([{host: '195.176.181.55', port: 6379}]);
 
-/*var client = redis.createClient({
-	database : 1,
-	usePromise: Promise
-});*/
 var states = {}
 exports.calcSlot = function(str){
 	return redis.calcSlot(str);
 }
 
-//exports.set = function(key, value, cb) {
-//	client.set(key,value)(function(error,res){ 
-//		if(error) throw error;  
-//		if(typeof cb == "function") { 
-//			cb(res); 
-//		} 
-//	});
-//}
+/*
+	Getter and setters without stringifying.
+*/
+exports.plainSet = function(key, value, cb){
+	client.set(key,value, function(error,res){ 
+		if(error) throw error;  
+		if(typeof cb == "function") { 
+				cb(res); 
+		} 
+	});
+}
 
+exports.plainGet = function(key, cb){
+	client.get(key, function(error,res){
+    	if(typeof cb == "function") { 
+    		cb(res); 
+    	} 
+    });
+}
+
+/*
+	Sets a value at a given key. 
+	If the key previously stored a value of another type (i.e., trying
+	to save a string on top of an integer), redis will throw an error.
+	
+	@param {string} key   : The key onto where to save the value.
+	@param {Object} value : The value to store (can be of any kind).
+	@param {Function} cb  : The callback function to be called. 
+*/
 exports.set = function(key, value, cb) {
 	if(isJSON(value)){
 		var cumulativeRes = [];
 		var f = flatten(value);
 		for(entry in f){
-	        client.hset(key,entry,f[entry], function(error,res){
+	        client.hset(key, entry, f[entry], function(error,res){
 	                if(error) throw error;
 	                cb(res);
 	        });
@@ -47,8 +64,14 @@ exports.set = function(key, value, cb) {
 	
 }
 
+/*
+	Gets the value stored at a given key.
+	
+	@param {string} key   : The key that holds the value we are interested into.
+	@param {Function} cb  : The callback function to be called. 
+*/
 exports.get = function(key, cb) {
-	client.type(key)(function(error,res){
+	client.type(key, function(error,res){
 		//console.log(error,res);
         if(error) throw error;
         var keyType = res;
@@ -60,6 +83,7 @@ exports.get = function(key, cb) {
     		});
     	}else{ //the retrived key match to a string
     		client.get(key, function(error,res){ 
+    			//console.log(key, error, res);
     			if(typeof cb == "function") { 
     				cb(res); 
     			} 
@@ -69,8 +93,16 @@ exports.get = function(key, cb) {
 	
 }
 
+/*
+	Increments the value stored at the key. If there is no value at the given key,
+	incr will behave as if 0 was stored. If a value which is not an integer is stored
+	at the key, an error will be thrown.
+	
+	@param {string} key   : The key that holds the value we want to increment.
+	@param {Function} cb  : The callback function to be called. 
+*/
 exports.incr = function(key, cb) {
-	client.incr(key)(function(err, response){
+	client.incr(key, function(err, response){
 		if(err) throw err;
 	    if(typeof cb == "function") {
 	    	cb(response);
@@ -78,8 +110,16 @@ exports.incr = function(key, cb) {
 	})
 }
 
+/*
+	Decrements the value stored at the key. If there is no value at the given key,
+	decr will behave as if 0 was stored. If a value which is not an integer is stored
+	at the key, an error will be thrown.
+	
+	@param {string} key   : The key that holds the value we want to decrement.
+	@param {Function} cb  : The callback function to be called. 
+*/
 exports.decr = function(key, cb) {
-	client.decr(key)(function(err, response){
+	client.decr(key, function(err, response){
 		if(err) throw err;
 	    if(typeof cb == "function") {
 	    	cb(response);
@@ -87,24 +127,70 @@ exports.decr = function(key, cb) {
 	})
 }
 
-exports.addToSortList = function(args, cb) {
-  /*client.zadd(args)(function(err, response) {
-  	  console.log(err, response);
-	  if(err) throw err;
-	  if(typeof cb == "function") {
-	    cb(response);
-	  }
-  });*/
+/*
+	Adds the specified members with the specified scores in a sorted set stored at key.
+	The args array should contain a score with a member, for example [1, "uno", 2, "due"]
+	where the strings are the members and their preceeding number is their score. This is 
+	stored in a sorted list at the given key which updates each time a new member with a new
+	score is added.
+	Remember to JSON.stringify() any object that you want to store in the sorted list, and to
+	JSON.parse() when these objects are retrieved.
+	
+	@param {string} setName  : The key that holds the sorted list.
+	@param {array} args      : The array of the form [score1, member1, score2, member2, ...] to be stored.
+	@param {function} cb     : The callback function to be called.
+*/
+exports.addToSortList = function(setName, args, cb) {
+  args.unshift(setName)
   client.zadd(args, function(err, res) {
-  	console.log(err, res);
   	if (err) throw err
-  	return res
+  	if(typeof cb == "function") {
+	    cb(res);
+	}
   });
 }
 
+/*
+	Returns the content of the sortlist stored with addToSortList function. If the scores
+	are to be displayed, the results are printed in a member, score, member, score... fashion
+	going from the highest score to the lowest score. If the scores are not to be displayed, 
+	the results will be printed with members only, showing from the highest scored member to
+	the lowest scored member.
+	If you previously stored objects inside the sorted list, remember to JSON.parse() them
+	before reading them.
+	
+	@param {string} setName : The name of the set to be returned.
+	@param {integer} start  : The start of the range to display the results.
+	@param {integer} end    : The end of the range to display the results (-1 goes until the end).
+	@param {bool} withScore : Boolean variable to tell the function if the scores are to be returned in the result.
+	@param {function} cb    : The callback function to be called.
+*/
+exports.getRangeSortList = function(setName, start, end, withScore, cb){
+  var sortedlist = [];
+  
+  if(!withScore) {
+    client.zrevrange(setName, start, end, function(err, res){
+    	if(err) throw err;
+    	cb(res);
+    });
+  }else{
+    client.zrevrange([setName, start, end, 'WITHSCORES'], function(err, res){
+    	if(err) throw err;
+    	cb(res);
+    });
+  }
+}
 
+/*
+	Increments by the given value the value associated with the given key in the given sorted list name.
+	
+	@param {string} setName : The name of the set where the value has to be incremented.
+	@param {integer} incr   : The increment for the given key.
+	@param {string} key     : The key whose value has to be incremented.
+	@param {function} cb    : The callback function to be called.
+*/
 exports.incrBySortList = function(setName, incr, key, cb) {
-  client.zincrby(setName, incr, key)(function(err, response){
+  client.zincrby(setName, incr, key, function(err, response){
     if(err) throw err;
     if(typeof cb == "function") {
     	cb(response);
@@ -112,23 +198,18 @@ exports.incrBySortList = function(setName, incr, key, cb) {
   })
 }
 
-exports.getRangeSortList = function(setName, start, end, withScore, cb){
-  var sortedlist = [];
-  
-  if(!withScore) {
-    client.zrevrange(setName, start, end)(cb);
-  }else{
-    client.zrevrange([setName, start, end, 'WITHSCORES'], function(err, res){
-    	console.log(err, res);
-    	if(err) throw err;
-    	cb(res);
-    });
-  }
-}
 
-
+/*
+	Pushes the value in the *fist* place of the list named key.
+	If you are pushing an Object, and not a primitive, be sure to JSON.stringify()
+	it before saving it, and to JSON.parse() it when reading it.
+	
+	@param {string} key   : The name of the list that has to receive the value.
+	@param {Object} value : The value to be pushed in the first slot of the list.
+	@param {function} cb  : The callback function to be called.
+*/
 exports.lpush = function(key, value, cb){
-	  client.lpush(key, value)(function(err, response) {
+	  client.lpush(key, value, function(err, response) {
 		  if(err) throw err;
 		  if(typeof cb == "function") {
 		    cb(response);
@@ -136,8 +217,18 @@ exports.lpush = function(key, value, cb){
 	  });
 }
 
+/*
+	Reads the list named key, from position start to position stop.
+	If you previously stored objects (and not primitives), be sure to JSON.parse()
+	them before reading/printing/sending them.
+	
+	@param {string} key     : The name of the set to be returned.
+	@param {integer} start  : The start of the range to display the results.
+	@param {integer} stop   : The end of the range to display the results (-1 goes until the end).
+	@param {function} cb    : The callback function to be called.
+*/
 exports.lrange = function(key, start, stop, cb){
-	  client.lrange(key, start, stop)(function(err, response) {
+	  client.lrange(key, start, stop, function(err, response) {
 		  if(err) throw err;
 		  if(typeof cb == "function") {
 		    cb(response);
@@ -145,22 +236,40 @@ exports.lrange = function(key, start, stop, cb){
 	  });
 }
 
+/*
+	Returns the length of the list.
+	
+	@param {string} key  : The name of the list.
+	@param {function} cb : The callback function to be called.
+*/
 exports.llen = function(key, cb){
-	  client.llen(key)(function(err, response) {
+	  client.llen(key, function(err, response) {
 		  if(err) throw err;
 		  if(typeof cb == "function") {
 		    cb(response);
 		  }
 	  });
 }
+
+/*
+	Trims the list stored at key and returns only the elements in the interval
+	[start, stop]. Keep in mind that the list is zero-based.
+	
+	@param {string} key    : The name of the list.
+	@param {integer} start : The start position of the interval to trim.
+	@param {integer} stop  : The final position of the interval to trim.
+	@param {function} cb   : The callback function to be called.
+*/
 exports.ltrim = function(key, start, stop, cb){
-	  client.ltrim(key, start, stop)(function(err, response) {
+	  client.ltrim(key, start, stop, function(err, response) {
 		  if(err) throw err;
 		  if(typeof cb == "function") {
 		    cb(response);
 		  }
 	  });
 }
+
+
 exports.getMergedSortList = function(cb){
 		var cb 
         var args = [];
@@ -168,8 +277,7 @@ exports.getMergedSortList = function(cb){
         		if(typeof arguments[i] == 'function') {
         			cb = arguments[i]
         		} else {
-        			 args = args.concat(client.zrevrange(arguments[i],0,9,"WITHSCORES")
-                             (function(error,res){ return Thunk(res); }));
+        			 args = args.concat(client.zrevrange(arguments[i],0,9,"WITHSCORES", function(error,res){ return Thunk(res); }));
         		}
         }
 
@@ -192,7 +300,7 @@ exports.getMergedSortList = function(cb){
                         })
 }
 
-function merge(a, b) {
+var merge = function(a, b) {
     var answer = [];
     while (a.length && b.length)
     {
@@ -205,7 +313,7 @@ function merge(a, b) {
     return answer;
 }
 
-function recursiveMerge(c,answer){
+var recursiveMerge = function(c, answer){
     if(!c.length) return answer
     else return recursiveMerge(c,merge(c.shift(),answer))
 }
@@ -237,14 +345,23 @@ var mergeAndSortList = function() {
         return sortByKey(temparray,1).slice(0,10);
 }
 
-function sortByKey(array, key) {
+/*
+	
+*/
+var sortByKey = function(array, key) {
     return array.sort(function(a, b) {
         var x = Number(a[key]); var y = Number(b[key]);
         return -1*((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 }
 
-function isJSON(value) {
+/*
+	[INTERNAL FUNCTION]
+	Returns a boolean indicating if the the passed value is a JSON or not.
+	
+	@param {object} value : Parameter to be checked.
+*/
+var isJSON = function(value) {
 	var stringConstructor = "test".constructor;
 	var arrayConstructor = [].constructor;
 	var objectConstructor = {}.constructor;
