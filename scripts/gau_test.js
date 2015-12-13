9c071d31e9d4f00d997c705f66d174942574472e
gaumuel <gauthier.mueller@gmail.com>
2015-12-13 18:32:05 +0100

    datepicker

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -7,9 +7,9 @@ k.createNode(function(msg){
 	msg = JSON.parse(msg);
 
 
-	// dataB.retrieveData(msg.id, msg.from, msg.to);
+	dataB.retrieveData(msg.id, msg.from, msg.to);
 
-	dataB.retrieveData(msg.id, 1449953366000,1449953378440)
+	// dataB.retrieveData(msg.id, 1449953366000,1449953378440)
 	// dataB.retrieveData(msg.id, 1449676695680,1449676698689)
 	//k.send(JSON.stringify(data));
 });
