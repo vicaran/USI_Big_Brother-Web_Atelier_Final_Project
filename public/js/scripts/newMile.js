e12732c65cfd4e44c4bda5d1cc0bc57f28069029
Andrea <andreavicari95@gmail.com>
2015-12-13 18:38:15 +0100

    gg

--- public/js/scripts/newMile.js
+++ public/js/scripts/newMile.js
@@ -52,18 +52,17 @@ var k = require('./../../../k_globals/koala.js')
 
 var htmlString = 
 // HEADER
-'<nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#page-top">Usi Big Brother</a>
-            </div>';
+'<nav class="navbar navbar-default navbar-fixed-top">'+
+        '<div class="container">'+
+            '<div class="navbar-header page-scroll">'+
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'+
+                    '<span class="sr-only">Toggle navigation</span>'+
+                    '<span class="icon-bar"></span>'+
+                    '<span class="icon-bar"></span>'+
+                    '<span class="icon-bar"></span>'+
+                '</button>'+
+                '<a class="navbar-brand" href="#page-top">Usi Big Brother</a>'+
+            '</div>';
 
            
 k.createHTML('pimmi', htmlString);
