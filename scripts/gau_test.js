a28b20328f8574e59026d3a8fb5d3ebe85271fb6
gaumuel <gauthier.mueller@gmail.com>
2015-12-13 15:05:57 +0100

    pikaday and moment.js

--- scripts/gau_test.js
+++ scripts/gau_test.js
@@ -3,13 +3,22 @@ var dataB = require('./database.js')
 
 //var data = {header: "database", content: "data from database"};
 k.createNode(function(msg){
+	function(){
+		var executed = false;
+		return function(){
+			if(!executed){
+				executed = true;
+				dataB.sendDataZero();
+			}
+		}
+	}
 	console.log("createNode " + msg);
 	msg = JSON.parse(msg);
 
 
 	// dataB.retrieveData(msg.id, msg.from, msg.to);
 
-	dataB.retrieveData(msg.id, 1449953366430,1449953378440)
+	dataB.retrieveData(msg.id, 1449953366000,1449953378440)
 	// dataB.retrieveData(msg.id, 1449676695680,1449676698689)
 	//k.send(JSON.stringify(data));
 });
