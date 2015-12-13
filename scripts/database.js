bab7fa84c6420e381fcd5e15dd66b0d71af49ba5
gaumuel <gauthier.mueller@gmail.com>
2015-12-13 15:19:02 +0100

    pikaday and moment.js

--- scripts/database.js
+++ scripts/database.js
@@ -51,8 +51,11 @@ var addToDatabase = function (data, d) {
         console.log("Since: ", since, " to: ", to)
         var startPoint =  since - oldest < 0 ? oldest : since - oldest;
         console.log(startPoint);
+
         startPoint = convertMStoS(startPoint)
-        var finishPoint = to - since;
+
+        var finishPoint = to - startPoint < 0? parse[parse.length -1 ].time: to - startPoint;
+
         finishPoint = convertMStoS(finishPoint)
         var toRetrieveData = [];
         var i = startPoint;
