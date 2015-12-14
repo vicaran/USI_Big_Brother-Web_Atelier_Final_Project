50e70be174d8496184e30421565a60bfc664108b
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-14 03:12:07 +0100

    S

--- public/js/scripts/app.js
+++ public/js/scripts/app.js
@@ -7,121 +7,119 @@ var tesselIds = {};
 var myLineChart;
 var myLineChart1;
 
-function check(data){
-    console.log('*******',data)
-    myLineChart.addData(data.time,[data.temperature,data.light,'porcodio'])
+function check(data) {
+    console.log('*******', data)
+    myLineChart.addData(data.time, [data.temperature, data.light, 'porcodio'])
 
 }
-window.onload = function () {
-    console.log('*********MADONNA NEGRA ***************** ')
-    //var updateGraphLine = function (volume, light, temp, time) {
-    //    //console.log(volume,light, time)
-    //    var date = new Date(time).toUTCString();
-    //    date = date.split(' ')[4];
-    //
-    //    myRealLine.destroy();
-    //
-    //
-    //    //push newly received data (time & data)
-    //    lineChartData1.datasets[0].data.push(volume);
-    //    document.getElementById('vol_2').innerHTML = volume;
-    //    lineChartData1.datasets[1].data.push(light);
-    //    document.getElementById('light_2').innerHTML = light;
-    //    lineChartData1.datasets[2].data.push(temp);
-    //    document.getElementById('temp_2').innerHTML = temp;
-    //    lineChartData1.labels.push(date);
-    //
-    //    //if longer than 20, remove the first one
-    //    if (lineChartData1.datasets[0].data.length > 10 | lineChartData1.datasets[1].data.length > 10 | lineChartData1.datasets[2].data.length > 10) {
-    //        lineChartData1.datasets[0].data.shift();
-    //        lineChartData1.datasets[1].data.shift();
-    //        lineChartData1.datasets[2].data.shift();
-    //        lineChartData1.labels.shift();
-    //    }
-    //
-    //    //draw it
-    //    myLineChart.Line(lineChartData1);
-    //
-    //    //empty the content of the div
-    //    //document.getElementById('newdata').setAttribute('volume', "");
-    //    //document.getElementById('newdata').setAttribute('time', "");
-    //}
+console.log('*********MADONNA NEGRA ***************** ')
+//var updateGraphLine = function (volume, light, temp, time) {
+//    //console.log(volume,light, time)
+//    var date = new Date(time).toUTCString();
+//    date = date.split(' ')[4];
+//
+//    myRealLine.destroy();
+//
+//
+//    //push newly received data (time & data)
+//    lineChartData1.datasets[0].data.push(volume);
+//    document.getElementById('vol_2').innerHTML = volume;
+//    lineChartData1.datasets[1].data.push(light);
+//    document.getElementById('light_2').innerHTML = light;
+//    lineChartData1.datasets[2].data.push(temp);
+//    document.getElementById('temp_2').innerHTML = temp;
+//    lineChartData1.labels.push(date);
+//
+//    //if longer than 20, remove the first one
+//    if (lineChartData1.datasets[0].data.length > 10 | lineChartData1.datasets[1].data.length > 10 | lineChartData1.datasets[2].data.length > 10) {
+//        lineChartData1.datasets[0].data.shift();
+//        lineChartData1.datasets[1].data.shift();
+//        lineChartData1.datasets[2].data.shift();
+//        lineChartData1.labels.shift();
+//    }
+//
+//    //draw it
+//    myLineChart.Line(lineChartData1);
+//
+//    //empty the content of the div
+//    //document.getElementById('newdata').setAttribute('volume', "");
+//    //document.getElementById('newdata').setAttribute('time', "");
+//}
 
-    function createGraph() {
+function createGraph() {
 
-        var ctx = document.getElementById("myChart").getContext("2d");
+    var ctx = document.getElementById("myChart").getContext("2d");
 
-        //Chart.defaults.global.responsive = true;
+    //Chart.defaults.global.responsive = true;
 
-        var data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-        Chart.defaults.global.responsive = true;
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+    Chart.defaults.global.responsive = true;
 
-        myLineChart = new Chart(ctx).Line(data);
-
-
-        var ctx1 = document.getElementById("myChart1").getContext("2d");
+    myLineChart = new Chart(ctx).Line(data);
 
 
-        myLineChart1 = new Chart(ctx1).Line(data);
-        console.log(myLineChart1)
+    var ctx1 = document.getElementById("myChart1").getContext("2d");
 
 
-    }
+    myLineChart1 = new Chart(ctx1).Line(data);
+    console.log(myLineChart1)
 
-    createGraph();
 
-    function createIdSelector() {
-        var container = document.getElementById("IdSelectorContainer");
-        for (var i = 0; i < idArray.length; i++) {
-            var newDiv = document.createElement('div');
-            newDiv.setAttribute('class', "IdSelector");
-            newDiv.setAttribute('data-id', idArray[i]);
-            var h2 = document.createElement('h2');
-            h2.innerHTML = idArray[i];
-            newDiv.appendChild(h2)
-            newDiv.addEventListener('click', function (e) {
-                var allDivs = document.getElementsByClassName('IdSelector activeIdSelector')
-                if (this.className == "IdSelector") {
-                    this.className = this.className + ' activeIdSelector'
-                    for (var i = 0; i < allDivs.length; i++) {
-                        if (allDivs[i] != this) {
-                            allDivs[i].className = "IdSelector"
-                        }
+}
+
+createGraph();
+
+function createIdSelector() {
+    var container = document.getElementById("IdSelectorContainer");
+    for (var i = 0; i < idArray.length; i++) {
+        var newDiv = document.createElement('div');
+        newDiv.setAttribute('class', "IdSelector");
+        newDiv.setAttribute('data-id', idArray[i]);
+        var h2 = document.createElement('h2');
+        h2.innerHTML = idArray[i];
+        newDiv.appendChild(h2)
+        newDiv.addEventListener('click', function (e) {
+            var allDivs = document.getElementsByClassName('IdSelector activeIdSelector')
+            if (this.className == "IdSelector") {
+                this.className = this.className + ' activeIdSelector'
+                for (var i = 0; i < allDivs.length; i++) {
+                    if (allDivs[i] != this) {
+                        allDivs[i].className = "IdSelector"
                     }
                 }
-                else {
-                    this.className = "IdSelector"
-                }
+            }
+            else {
+                this.className = "IdSelector"
+            }
 
 
-            });
-            container.appendChild(newDiv)
-        }
+        });
+        container.appendChild(newDiv)
     }
-    createIdSelector()
+}
+createIdSelector()
 
-};
