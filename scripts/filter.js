e8f045d35d28bf198de4a965ba5ff32d1fa8a4f1
FrancescoZuppichini <francesco.zuppichini@gmail.com>
2015-12-16 14:11:07 +0100

    s

--- scripts/filter.js
+++ scripts/filter.js
@@ -2,7 +2,7 @@ var k = require('./../k_globals/koala.js')
 var dataB = require('./database.js')
 
 k.createNode(function(msg){
-	console.log("createNode *S*DAHVHDKSHJGDGSHGJKDSAKJADS " + msg);
+	console.log("createNode" + msg);
 	msg = JSON.parse(msg);
 
 	dataB.retrieveData(msg.id, msg.from, msg.to);
